f970600aea2c35b4023fd434c712320667373f57
alerameli <rae.alejandro@gmail.com>
2023-01-27 18:42:25 -0700

    Updates

---
+++ precache-manifest.2589f7bb95fa2820cec9629568145a77.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "594ced601dc458ea6c6a",
+    "url": "/static/js/main.594ced60.chunk.js"
+  },
+  {
+    "revision": "a6d11353c5d249d8c561",
+    "url": "/static/js/1.a6d11353.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "541438fcd8282426ddec4e890a3b2f37",
+    "url": "/index.html"
+  }
+];
