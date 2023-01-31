feddb021db6451b97f0dec3fa677d25592f79aab
alerameli <rae.alejandro@gmail.com>
2023-01-30 17:40:13 -0700

    Updates

---
+++ precache-manifest.6a35fa1cfeb14301c8ff08c629abf701.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "530185e11968e60a6f21",
+    "url": "/front-end/static/js/main.530185e1.chunk.js"
+  },
+  {
+    "revision": "2f942557141fdef843c7",
+    "url": "/front-end/static/js/1.2f942557.chunk.js"
+  },
+  {
+    "revision": "d18b3840bf4161e78d54",
+    "url": "/front-end/static/js/runtime~main.d18b3840.js"
+  },
+  {
+    "revision": "bcd27bf5b2891cf9217b98a499230e6c",
+    "url": "/front-end/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4869028a714a312a2a66dba1b751d1f.js"
+  "/front-end/precache-manifest.6a35fa1cfeb14301c8ff08c629abf701.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/front-end/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
